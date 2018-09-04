79237f3d0a5f747bbb32dbcc5dec80f3ae923a25
Corey Lee <zeros_tang@163.com>
2018-09-04 19:15:41 +0800

    modify orm.py, test model success.

--- www/orm.py
+++ www/orm.py
@@ -18,7 +18,7 @@ async def create_pool(loop, **kw):
         user = kw['user'],
         password = kw['password'],
         db = kw['db'],
-        charset = kw.get('charset', 'utf-8'),
+        charset = kw.get('charset', 'utf8'),
         autocommit = kw.get('autocommit', True),
         maxsize = kw.get('maxsize', 10),
         minsize = kw.get('minsize', 1),
@@ -45,6 +45,8 @@ async def execute(sql, args, autocommit=True):
             await conn.begin()
         try:
             async with conn.cursor(aiomysql.DictCursor) as cur:
+                print('------------------------------sql: ', sql)
+                print('------------------------------args: ', args)
                 await cur.execute(sql.replace('?', '%s'), args or ())
                 affected = cur.rowcount
             if not autocommit:
@@ -72,8 +74,8 @@ class Field(object):
         return '<%s, %s:%s' % (self.__class__.__name__, self.column_type, self.name)
 
 class StringField(Field):
-    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)')
-    super().__init__(name, ddl, primary_key, default)
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
 
 class BooleanField(Field):
     def __init__(self, name=None, default=False):
@@ -94,13 +96,13 @@ class TextField(Field):
 
 class ModelMetaclass(type):
     def __new__(cls, name, bases, attrs):
-        if name = 'Model':
+        if name == 'Model':
             return type.__new__(cls, name, bases, attrs)
         tableName = attrs.get('__table__', None) or name
-        logging.info('found model: %s (table: %s)' %s (name, tableName))
+        logging.info('found model: %s (table: %s)' % (name, tableName))
         mappings = dict()
         fields = []
-        primary_key = None
+        primarykey = None
         for k, v in attrs.items():
             if isinstance(v, Field):
                 logging.info('  found mapping: %s ==> %s' % (k, v))
@@ -117,15 +119,15 @@ class ModelMetaclass(type):
         for k in mappings.keys():
             attrs.pop(k)
 
-        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        escaped_fields = list(map(lambda f: '%s' % f, fields))
         attrs['__mappings__'] = mappings     # 保存属性和列的映射关系
         attrs['__table__'] = tableName
         attrs['__primary_key__'] = primaykey # 主键属性名
         attrs['__field__'] = fields   # 除主键外的属性名
-        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaykey, ', '.join(escaped_fields), tableName)
-        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaykey, create_args_string(len(escaped_fields) + 1))
-        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaykey)
-        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaykey)
+        attrs['__select__'] = 'select %s, %s from %s' % (primaykey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into %s (%s, %s) values (%s)' % (tableName, ', '.join(escaped_fields), primaykey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update %s set %s where %s=?' % (tableName, ', '.join(map(lambda f: '%s=?' % (mappings.get(f).name or f), fields)), primaykey)
+        attrs['__delete__'] = 'delete from %s where %s=?' % (tableName, primaykey)
         return type.__new__(cls, name, bases, attrs)
 
 
@@ -155,7 +157,7 @@ class Model(dict, metaclass=ModelMetaclass):
                 value = field.default() if callable(field.default) else field.default
                 logging.debug('using default value for %s: %s' % (key, str(value)))
                 setattr(self, key, value)
-            return value
+        return value
 
 
 
@@ -189,7 +191,7 @@ class Model(dict, metaclass=ModelMetaclass):
     @classmethod
     async def findNumber(cls, seletField, where=None, args=None):
             ' find number by select and where. '
-            sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+            sql = ['select %s _num_ from %s' % (selectField, cls.__table__)]
             if where:
                 sql.append('where')
                 sql.append(where)
@@ -201,13 +203,14 @@ class Model(dict, metaclass=ModelMetaclass):
     @classmethod
     async def find(cls, pk):
         ' find object by primay key. '
-        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        rs = await select('%s where %s=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
         if len(rs) == 0:
             return None
         return cls(**rs[0])
 
     async def save(self):
         args = list(map(self.getValueOrDefault, self.__field__))
+        print('***********--- args: ', args)
         args.append(self.getValueOrDefault(self.__primary_key__))
         rows = await execute(self.__insert__, args)
         if rows != 1:
